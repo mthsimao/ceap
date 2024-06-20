7b58470469174b539ee79b8c72c8bfe800e4792f
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2024-06-19 21:09:27 -0300

    improving

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let dropdown1 = document.querySelector('.dropdown')
-let dropdown_content = document.querySelector('.dropdown-content')
 let dropdown_click1 = document.querySelector('.dropdown-click')
 
-dropdown_click1.addEventListener('click', () =>{
+
+dropdown_click1.addEventListener('click', () => {
     dropdown1.classList.toggle('show')
 })
